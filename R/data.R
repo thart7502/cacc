e2990bca6bf57c362f79db6669a2d19eb8697ca5
amoneva <amonevapardo@gmail.com>
2022-07-26 16:36:54 +0200

    Document test data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Test data to compute a Conjunctive Analysis of Case Configurations
+#'
+#' A dataset containing synthetic example data to conduct a Conjunctive Analysis of Case Configurations.
+#'
+#' @format A data frame with 100 rows and 9 variables:
+#' \describe{
+#'   \item{iv1, iv2, iv3, iv4}{Example integer independent variables.}
+#'   \item{iv1_c, iv2_c, iv3_c, iv4_c}{Example character or factor independent variables.}
+#'   \item{dv1}{Example integer, dichotomous, dependent variable.}
+#' }
+"test_data"
